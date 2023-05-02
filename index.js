b42c7b2f8a257f948c48dacb18ef5258f969e0e3
ranktel <ranktel@worktop.myguest.virtualbox.org>
2023-05-02 11:10:14 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -16,9 +17,53 @@ app.get('/', async (req, res, next) => {
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post('/register', async (req, res, next) => {
+	try{
+		
+		let userparams = req.body;
+		userparams["password"] = await bcrypt.hash(userparams["password"], 2);
+		
+		let createdUser = await User.create(userparams);
+
+		
+		res.send("successfully created user " + createdUser.username);
+		res.status = 200;
+
+	}catch(error){
+		console.error(error)
+		next(error)
+	}
+});
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post("/login", async (req, res, next) => {
+	try{
+		let userparams = req.body;
+	
+		let result = await User.findOne({
+			where: {
+				username: userparams["username"]
+			}
+		});
+	
+//		let trialword = await bcrypt.hash(userparams["password"], 2);
+		
+		//console.log(trialword)
+		console.log(result.password);
 
+		let trueOrNot = await bcrypt.compare( userparams.password, result.password);
+			
+		if(trueOrNot == true){
+
+			res.send("successfully logged in user " + result.username);
+		}else{
+			res.send("incorrect username or password");
+		}
+	}catch(error){
+		console.error(error);
+		next(error);
+	}
+})
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
